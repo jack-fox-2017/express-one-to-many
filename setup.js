7a120a49a637b31d724e661c0a3dae94e4730096
Renataclara <renataclarak@gmail.com>
2017-08-07 18:32:25 +0700

    done

--- setup.js
+++ setup.js
@@ -1,26 +1,84 @@
-const sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./db/data.db');
+'use strict'
 
-// function createTable() {
-db.serialize(function() {
-  db.run(`CREATE TABLE if not exists 'Contact' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' text, 'company' text, 'phone' INTEGER, 'email' text)`);
-  console.log('Contact Table Created!');
+var sqlite3 = require('sqlite3').verbose() // lbh detailin kalo ada error// kalo da production fileny di ilangin
+var db = new sqlite3.Database('./db/data.db')
 
-  db.run(`CREATE TABLE if not exists 'Grup' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' text)`)
-  console.log('Group Table Created!');
+function createTable(){
+  // db.run(`CREATE TABLE IF NOT EXISTS Contacts (
+  // id INTEGER primary key AUTOINCREMENT,
+  // name varchar(50),
+  // company varchar(50),
+  // telp_number varchar(15),
+  // email varchar(50)
+  // );`)
 
-  db.run(`CREATE TABLE if not exists 'Profile' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'username' text, 'password' text, 'firstname' text, 'lastname' text, 'ContactId' INTEGER, FOREIGN KEY(ContactId) REFERENCES Contact(id))`);
-  console.log('Profile Table Created!');
+  db.run(`CREATE TABLE IF NOT EXISTS Groups (
+  id INTEGER primary key AUTOINCREMENT,
+  name_of_group varchar(50)
+  );`)
 
-  db.run(`CREATE TABLE if not exists 'Address' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'address' text, 'city' text, 'zipcode' INTEGER, 'ContactId' INTEGER, FOREIGN KEY(ContactId) REFERENCES Contact(id))`);
-  console.log('Address Table Created!');
+  db.run(`CREATE TABLE IF NOT EXISTS Addresses (
+  id INTEGER primary key AUTOINCREMENT,
+  street varchar(50),
+  city varchar(50),
+  zip_code varchar(50),
+  contacts_id INTEGER
+  );`)
 
-  db.run(`CREATE TABLE if not exists 'CG' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'ContactId' INTEGER, 'GroupId' INTEGER)`)
-})
+  // db.run(`CREATE TABLE IF NOT EXISTS Profiles (
+  // id INTEGER primary key AUTOINCREMENT,
+  // username varchar(50),
+  // password varchar(50),
+  // contacts_id INTEGER
+  // );`)
+
+  // db.run(`CREATE TABLE IF NOT EXISTS Profiles (
+  // id INTEGER primary key AUTOINCREMENT,
+  // username varchar(50),
+  // password varchar(50),
+  // contacts_id INTEGER
+  // );`)
 
 
-function insertData() {
-  db.run("INSERT INTO CONTACT (name, company, phone, email) VALUES ('Achim Baggins', 'Hacktiv8 Indonesia', 081803704343, 'achim_baggins@yahoo.com')");
+  db.serialize(function() {
+    db.run(`CREATE TABLE IF NOT EXISTS Contacts (
+    id INTEGER primary key AUTOINCREMENT,
+    name varchar(50),
+    company varchar(50),
+    telp_number varchar(15),
+    email varchar(50)
+    );`)
+
+    db.run(`CREATE TABLE IF NOT EXISTS Profiles (
+    id INTEGER primary key AUTOINCREMENT,
+    username varchar(50),
+    password varchar(50),
+    contacts_id INTEGER UNIQUE, FOREIGN KEY(contacts_id) REFERENCES Contacts(id)
+    );`)
+  });
+
+  db.run(`CREATE TABLE IF NOT EXISTS ContactGroups (
+  id INTEGER primary key AUTOINCREMENT,
+  contacts_id INTEGER,
+  groups_id INTEGER
+  );`)
 }
-// createTable()
-// insertData()
+
+function dropTableContacts() {
+  db.run(`DROP TABLE Contacts`);
+}
+
+function dropTableGroups() {
+  db.run(`DROP TABLE Groups`);
+}
+
+function dropTableProfiles() {
+  db.run(`DROP TABLE Profiles`);
+}
+
+function dropTableAddresses() {
+  db.run(`DROP TABLE Addresses`);
+}
+
+createTable();
+// dropTableProfiles();
